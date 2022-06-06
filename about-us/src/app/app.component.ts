8a2af714d2031e5f2f2c4b76ab8e086d8e4e255c
developerashish17 <yadavashish0798@gmail.com>
2022-06-06 09:22:33 +0530

    updated

--- about-us/src/app/app.component.ts
+++ about-us/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'about-us';
+  userList: any = [];
+
+  constructor(
+    private users: UsersService
+  ) { }
+
+  ngOnInit(): void {
+    this.getUsersListing();
+  }
+
+  getUsersListing() {
+    this.users.getUsers().subscribe(res => {
+      this.userList = res;
+      console.log('userlist', this.userList)
+    });
+  };
+
 }
+
